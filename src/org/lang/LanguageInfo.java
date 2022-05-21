e519b1a9255eccba3226a67d33072fc673d0df21
shanmathibaskaran <shanmathibaskara1996@gmail.com>
2022-05-21 13:36:26 +0530

    code

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,26 @@
+package org.lang;
+
+public class LanguageInfo extends StateDetails {
+	public void tamilLanguage() {
+		System.out.println("tamil");
+
+	}
+	public void englishLanguage() {
+
+		System.out.println("english");
+	}
+	public void hindiLanguage() {
+
+		System.out.println("hindi");
+	}
+	public static void main(String[] args) {
+		LanguageInfo l =new LanguageInfo();
+		l.tamilLanguage();
+		l.englishLanguage();
+		l.hindiLanguage();
+		l.southIndia();
+		l.northIndia();
+		
+
+	}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,15 @@
+package org.lang;
+
+public class StateDetails {
+	public void southIndia() {
+
+		System.out.println("SouthIndia language is:Tamil");
+
+	}
+	public void northIndia() {
+
+		System.out.println("NorthIndia language is:Hindi");
+
+	}
+	
+}
